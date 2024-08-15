cf4ccc2f8744f5fa8aa8fca168ea8103efdfd40d
박기현 <kihyun1998@penta.co.kr>
2024-08-15 18:07:01 +0900

    Update: 알림 메시지 개선

--- main.go
+++ main.go
@@ -73,27 +73,29 @@ func generateDescription(signalResult lib.SignalResult) string {
 
 	description := fmt.Sprintf("Signal: %s for BTCUSDT at %s\n\n", signalResult.Signal, timestamp)
 	description += fmt.Sprintf("Price : %.3f\n", signalResult.Price)
-	description += fmt.Sprintf("Stoploss : %.3f, Takeprofit: %.3f\n", signalResult.StopLoss, signalResult.TakeProfie)
+	description += fmt.Sprintf("Stoploss : %.3f, Takeprofit: %.3f\n\n", signalResult.StopLoss, signalResult.TakeProfie)
 
-	description += "[LONG]\n"
+	description += "=======[LONG]=======\n"
 	description += fmt.Sprintf("[EMA200] : %v \n", signalResult.Conditions.Long.EMA200Condition)
-	description += fmt.Sprintf("EMA200: %.3f, Diff: %.3f\n", signalResult.Conditions.Long.EMA200Value, signalResult.Conditions.Long.EMA200Diff)
+	description += fmt.Sprintf("EMA200: %.3f, Diff: %.3f\n\n", signalResult.Conditions.Long.EMA200Value, signalResult.Conditions.Long.EMA200Diff)
 
 	description += fmt.Sprintf("[MACD] : %v \n", signalResult.Conditions.Long.MACDCondition)
-	description += fmt.Sprintf("MACD Line: %.3f, Signal Line: %.3f, Histogram: %.3f\n", signalResult.Conditions.Long.MACDNowMACDLine, signalResult.Conditions.Long.MACDNowSignalLine, signalResult.Conditions.Long.MACDHistogram)
+	description += fmt.Sprintf("MACD Line: %.3f, Signal Line: %.3f, Histogram: %.3f\n\n", signalResult.Conditions.Long.MACDNowMACDLine, signalResult.Conditions.Long.MACDNowSignalLine, signalResult.Conditions.Long.MACDHistogram)
 
 	description += fmt.Sprintf("[Parabolic SAR] : %v \n", signalResult.Conditions.Long.ParabolicSARCondition)
-	description += fmt.Sprintf("ParabolicSAR: %.3f, Diff: %.3f\n", signalResult.Conditions.Long.ParabolicSARValue, signalResult.Conditions.Long.ParabolicSARDiff)
+	description += fmt.Sprintf("ParabolicSAR: %.3f, Diff: %.3f\n\n", signalResult.Conditions.Long.ParabolicSARValue, signalResult.Conditions.Long.ParabolicSARDiff)
+	description += "=====================\n\n"
 
-	description += "[SHORT]\n"
+	description += "=======[SHORT]=======\n"
 	description += fmt.Sprintf("[EMA200] : %v \n", signalResult.Conditions.Short.EMA200Condition)
-	description += fmt.Sprintf("EMA200: %.3f, Diff: %.3f\n", signalResult.Conditions.Short.EMA200Value, signalResult.Conditions.Short.EMA200Diff)
+	description += fmt.Sprintf("EMA200: %.3f, Diff: %.3f\n\n", signalResult.Conditions.Short.EMA200Value, signalResult.Conditions.Short.EMA200Diff)
 
 	description += fmt.Sprintf("[MACD] : %v \n", signalResult.Conditions.Short.MACDCondition)
-	description += fmt.Sprintf("MACD Line: %.3f, Signal Line: %.3f, Histogram: %.3f\n", signalResult.Conditions.Short.MACDNowMACDLine, signalResult.Conditions.Short.MACDNowSignalLine, signalResult.Conditions.Short.MACDHistogram)
+	description += fmt.Sprintf("MACD Line: %.3f, Signal Line: %.3f, Histogram: %.3f\n\n", signalResult.Conditions.Short.MACDNowMACDLine, signalResult.Conditions.Short.MACDNowSignalLine, signalResult.Conditions.Short.MACDHistogram)
 
 	description += fmt.Sprintf("[Parabolic SAR] : %v \n", signalResult.Conditions.Short.ParabolicSARCondition)
-	description += fmt.Sprintf("ParabolicSAR: %.3f, Diff: %.3f\n", signalResult.Conditions.Short.ParabolicSARValue, signalResult.Conditions.Short.ParabolicSARDiff)
+	description += fmt.Sprintf("ParabolicSAR: %.3f, Diff: %.3f\n\n", signalResult.Conditions.Short.ParabolicSARValue, signalResult.Conditions.Short.ParabolicSARDiff)
+	description += "=====================\n"
 
 	return description
 }
